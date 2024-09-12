d3b252768614c9ad094dbb7b9eff18c9c48df659
suvapor283 <suvapor8632@gmail.com>
2024-09-12 23:33:41 +0900

    목록

--- src/com/ll/App.java
+++ src/com/ll/App.java
@@ -31,6 +31,14 @@ public class App {
 				quotations.add(new Quotation(++lastQuotationId, quotation, writer));
 				System.out.println(lastQuotationId + "번 명언이 등록되었습니다.");
 			}
+			
+			else if (cmd.equals("목록")) {
+				System.out.println("번호	|	작가	|	명언");
+				for(int i = quotations.size()-1; i >= 0; i--) {
+					Quotation quotation = quotations.get(i);
+					System.out.printf("%d	|	%s	|	%s\n", quotation.id, quotation.quotation, quotation.writer);
+				}
+			}
 		}
 
 		sc.close();
