6f599679467ba7f5ad83fa99c0fdf96c8a006065
suvapor283 <suvapor8632@gmail.com>
2024-09-16 22:56:03 +0900

    삭제 - 존재하지 않는 게시물 처리

--- src/com/ll/App.java
+++ src/com/ll/App.java
@@ -8,7 +8,7 @@ public class App {
 
 	public void run() {
 		Scanner sc = new Scanner(System.in);
-		
+
 		List<Quotation> quotations = new ArrayList<>();
 		int lastQuotationId = 0;
 
@@ -21,41 +21,55 @@ public class App {
 			if (cmd.equals("종료")) {
 				break;
 			}
-			
+
 			if (cmd.equals("등록")) {
 				System.out.print("명언 : ");
 				String quotation = sc.nextLine().trim();
 				System.out.print("작가 : ");
 				String writer = sc.nextLine().trim();
-				
+
 				quotations.add(new Quotation(++lastQuotationId, quotation, writer));
 				System.out.println(lastQuotationId + "번 명언이 등록되었습니다.");
 			}
-			
+
 			else if (cmd.equals("목록")) {
 				if (quotations.isEmpty()) {
 					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
-				
+
 				System.out.println("번호	|	작가	|	명언");
-				for(int i = quotations.size()-1; i >= 0; i--) {
+				for (int i = quotations.size() - 1; i >= 0; i--) {
 					Quotation quotation = quotations.get(i);
 					System.out.printf("%d	|	%s	|	%s\n", quotation.id, quotation.quotation, quotation.writer);
 				}
 			}
-			
+
 			else if (cmd.startsWith("삭제?id=")) {
 				int id = -1;
-				
+
 				try {
 					id = Integer.parseInt(cmd.substring(6));
 				} catch (NumberFormatException e) {
 					System.out.println("명령어를 올바르게 입력해주세요.");
 					continue;
 				}
-				
-				quotations.remove(id);
+
+				Quotation foundQuotation = null;
+
+				for (Quotation quotation : quotations) {
+					if (quotation.id == id) {
+						foundQuotation = quotation;
+						break;
+					}
+				}
+
+				if (foundQuotation == null) {
+					System.out.println(id + "번 게시물은 존재하지 않습니다.");
+					continue;
+				}
+
+				quotations.remove(foundQuotation);
 				System.out.println(id + "번 게시물이 삭제되었습니다.");
 			}
 		}
